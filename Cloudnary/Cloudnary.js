c2896e943dbd4465098169250b8ebc00c01e15b8
mannumishra <mannumishra2207@outlook.com>
2024-07-06 10:54:39 +0530

    add chnages

--- Cloudnary/Cloudnary.js
+++ Cloudnary/Cloudnary.js
@@ -1,9 +1,9 @@
 const cloudinary = require('cloudinary').v2
 
 cloudinary.config({
-    cloud_name: "dsimn9z1r",
-    api_key: "998919427255124",
-    api_secret: "h-PsVovtSvzakWubj1X8sXJEtp4"
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECTER
 })
 
 
